a8224b8848adfa9d0637f59d2d90584d989a3d89
savagewebdev <rbnsavage@gmail.com>
2017-09-09 17:26:19 -0500

    Variable tinkering

--- designs.js
+++ designs.js
@@ -3,21 +3,19 @@ let n;
 
 n = function() {
 	$("input:first").val();
-	return;
 }
 
 let m;
 
 m = function() {
 	$("input:second").val(); // value? 
-	return;
 }	
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
-	for (var i = 1; i < n; i++) {
+	for (let i = 1; i < n; i++) {
 		$("#pixel_canvas").append("<tr> </tr>"); // For each input number, create a row.
-		for (var l = 1; l < m; l++) {
+		for (let l = 1; l < m; l++) {
 			$("tr").append("<td> </td>"); // For each row, create a column (data).
 		}	
 	}
