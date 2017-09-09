fda63833ed608d1eb815f3901cfc8a1e60b3e7dc
savagewebdev <rbnsavage@gmail.com>
2017-09-08 20:04:50 -0500

    Tinkering without help

--- designs.js
+++ designs.js
@@ -1,16 +1,33 @@
 // Select size input
-const n = $("input:first" ).val(); // value? () === "correct" ) { var n and m equal the parameters for makeGrid?
-const m = $("input:second").val(); // value? 	
-	
-	
-// Select color input
-$("#colorPicker").on(function(event) {
-	return;
+let n = function() {
+	$("input:first").val();
+}
+// value? () === "correct" ) { var n and m equal the parameters for makeGrid?
+let m = function() {
+	$("input:second").val(); // value? 
+}	
+
+function buttonLove() {
+	$("form#sizePicker").submit(makeGrid());
 }
 
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid(n, m) {
-$("form").submit(function(event) {
-event.preventDefault();
+function makeGrid() {
+	for (var i = 1; i < n; i++) {
+		$("#pixel_canvas").append("<tr> </tr>"); // For each input number, create a row.
+		for (var l = 1; l < m; l++) {
+			$("tr").append("<td> </td>");
+		}	
+	}
 }
+
+
+
+// Select color input
+//$("#colorPicker").on(function(event) {
+//	return;
+
+
+
+
+	
