685bb6a162fb27e434d45aa1c174cdd60f70168d
savagewebdev <rbnsavage@gmail.com>
2017-09-14 16:50:10 -0500

    Tests with Karol

--- designs.js
+++ designs.js
@@ -1,26 +1,32 @@
 // Select size input
 
-var n = function() {
+var width = function() {
 	$("#input_height:input").val();
+	return;
 }
 
-var m = function() {
-	$("#input_width:input").val(); // value? 
-}	
 
 // When size is submitted by the user, call makeGrid()
-function makeGrid() {
-	for (var i = 1; i < n; i++) {
+function makeGrid(width, height) {
+	
+	for (var i = 0; i < width; i++) {
 		$("#pixel_canvas").append("<tr> </tr>"); // For each input number, create a row.
-		for (var l = 1; l < m; l++) {
+		for (var l = 0; l < height; l++) {
 			$("tr").append("<td> </td>"); // For each row, create a column (data).
 		}	
 	}
 }
 
-function buttonLove() { // Used to initialize the makeGrid function.
-	$("form").submit(makeGrid());
-}
+
+$("form").submit(function(event) {
+	event.preventDefault();
+	//console.log("Test");
+	let width = $("#input_width").val(); 
+	let height = $("#input_height").val();
+	makeGrid(width, height);
+	
+});
+
 
 
 // Select color input
