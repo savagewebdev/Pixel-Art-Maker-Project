caac78c61ac82947cfd24a1245f07ca426ec8969
savagewebdev <rbnsavage@gmail.com>
2017-09-13 20:34:03 -0500

    Test modifications

--- designs.js
+++ designs.js
@@ -1,21 +1,18 @@
 // Select size input
-let n;
 
-n = function() {
-	$("#input_height").val();
+var n = function() {
+	$("#input_height:input").val();
 }
 
-let m;
-
-m = function() {
-	$("#input_width").val(); // value? 
+var m = function() {
+	$("#input_width:input").val(); // value? 
 }	
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
-	for (let i = 1; i < n; i++) {
+	for (var i = 1; i < n; i++) {
 		$("#pixel_canvas").append("<tr> </tr>"); // For each input number, create a row.
-		for (let l = 1; l < m; l++) {
+		for (var l = 1; l < m; l++) {
 			$("tr").append("<td> </td>"); // For each row, create a column (data).
 		}	
 	}
