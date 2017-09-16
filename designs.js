595a7ef06c2c16c27e609604a02db6d25558969b
savagewebdev <rbnsavage@gmail.com>
2017-09-16 16:05:21 -0500

    Commentary

--- designs.js
+++ designs.js
@@ -1,14 +1,16 @@
 // Select size input
 
-var width = function() {
-	$("#input_height:input").val();
+var height = function() { // The height variable defined as a function
+	$("#input_height:input").val(); // which selects an input box with the ID of #input_height and produces an input value via method
+	return; // and returns it for later use.
+}
+
+var width = function() { // Same as above, but defining width.
+	$("#input_width:input").val();
 	return;
 }
 
-
-// When size is submitted by the user, call makeGrid()
-function makeGrid(width, height) {
-	
+function makeGrid(width, height) { // The function in charge of creating the grid using the above axis vars as parameters. The axis variables help to determine the extent of the "for" loops.
 	for (var i = 0; i < width; i++) {
 		$("#pixel_canvas").append("<tr> </tr>"); // For each input number, create a row.
 		for (var l = 0; l < height; l++) {
@@ -17,16 +19,13 @@ function makeGrid(width, height) {
 	}
 }
 
-
-$("form").submit(function(event) {
-	event.preventDefault();
-	//console.log("Test");
-	let width = $("#input_width").val(); 
-	let height = $("#input_height").val();
-	makeGrid(width, height);
-	
+$("form").submit(function(event) { // The function enacted via jquery when the submit button is pressed.
+	event.preventDefault(); // First, prevent the form from being submitted.
+	let width = $("#input_width").val(); // 
+	let height = $("#input_height").val(); // Locally, the variables are defined as the DOM's input values...
+	makeGrid(width, height); // and are used as arguments in calling the makeGrid function.
 });
-
+// Thus the "submit" event & jquery function simply draws upon the makeGrid function for its actions, which in turn uses the height and width inputs for two reasons: As variables determining the limits of the "for" loop, and as local parameters in the submit event. They cannot be shared due to the separate scope of the two functions, but they do both draw upon the same source--the input data itself. 
 
 
 // Select color input
